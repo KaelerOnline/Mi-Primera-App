5b089fd6f32ba97723a820a8952a887795154c7c
Marco <kaeleronlinefartoomuch@gmail.com>
2018-05-02 12:54:31 +0200

    Commit Nº1 Añadidos a la APP imagenes .gif, botones con intents implícitos (Uno para abrir una URL, otro para abrir el teléfono con el número 1234 y otro que lleve a una segunda Activity) y logs para debug. Falta comentar correctamente.

--- MiPrimeraApp/app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
+++ MiPrimeraApp/app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
@@ -1,7 +1,16 @@
 package com.teaching.android.miprimeraapp;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ActionMenuView;
+
+import static android.content.Intent.ACTION_DIAL;
+import static android.content.Intent.ACTION_VIEW;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +19,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","MARCO: onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity","MARCO: onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","MARCO: onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","MARCO: onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("MainActivity","MARCO: onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","MARCO: onDestroy");
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void linkURL(View view){
+        String url = "https://www.google.com";
+        Intent intent = new Intent(ACTION_VIEW, Uri.parse(url));
+        Log.d("MainActivity","MARCO: URL");
+        startActivity(intent);
+    }
+
+    public void numero(View view){
+        Intent intent = new Intent(ACTION_DIAL, Uri.parse("tel: 1234"));
+        Log.d("MainActivity","MARCO: tel: 1234");
+        startActivity(intent);
+    }
+
+
 }
---
+++ MiPrimeraApp/app/src/main/java/com/teaching/android/miprimeraapp/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.teaching.android.miprimeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
